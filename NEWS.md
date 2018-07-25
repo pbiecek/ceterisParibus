6b2698b2d5819d2ab3d045daa176d631c9c7a04a
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-07-25 12:27:20 +0200

    new ceteris paribus function

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,12 @@
 ceterisParibus 0.3
 ----------------------------------------------------------------
-Major refactoring of the code
 
 * The `ceteris_paribus()` function from ver 0.2 is now avaliable as `what_if()`. Here is where the development of older version will take place.
 
+Major refactoring of the code
+----------------------------------------------------------------
+
+Please note, that `what_if()` and `local_fit()` functions are	remainders from version 0.2 and they will be deprecated in version 0.4. 
+They will not be avaliable in version 1.0. 
+From version 0.3 the recommended way to create explainers is through the function `ceteris_paribus()`.
 
---
+++ R/ceteris_paribus.R
@@ -0,0 +1,61 @@
+#' Ceteris Paribus Explainer
+#'
+#' This function calculate ceteris paribus profiles for selected data points.
+#'
+#' @param explainer a model to be explained, preprocessed by function `DALEX::explain()`.
+#' @param variable_splits named list of splits for variables, in most cases created with `calculate_variable_splits()`. If NULL then it will be calculated based on validation data avaliable in the `explainer`.
+#' @param observations set of observarvation for which profiles are to be calculated
+#' @param grid_points number of points for profile. Will be passed to `calculate_variable_splits()`.
+#' @param variables names of variables for which profiles shall be calculated. Will be passed to `calculate_variable_splits()`. If NULL then all variables from the validation data will be used.
+#'
+#' @return An object of the class 'ceteris_paribus_explainer'.
+#' It's a data frame with calculated average responses.
+#' @export
+#'
+#' @examples
+#' library("DALEX")
+#' library("randomForest")
+#' set.seed(59)
+#'
+#' apartments_rf_model <- randomForest(m2.price ~ construction.year + surface + floor +
+#'       no.rooms + district, data = apartments)
+#'
+#' explainer_rf <- explain(apartments_rf_model,
+#'       data = apartmentsTest[,2:6], y = apartmentsTest$m2.price)
+#'
+#' apartments_small <- select_sample(apartmentsTest, 10)
+#'
+#' cp_rf <- ceteris_paribus(explainer_rf, apartments_small)
+#' cp_rf
+ceteris_paribus <- function(explainer, observations, variable_splits = NULL, variables = NULL, grid_points = 101) {
+  if (!("explainer" %in% class(explainer)))
+      stop("The ceteris_paribus() function requires an object created with explain() function.")
+
+  predict_function <- explainer$predict_function
+  model <- explainer$model
+
+  # if splits are not provided, then will be calculated
+  if (is.null(variable_splits)) {
+    # need validation data from the explainer
+    if (is.null(explainer$data))
+      stop("The ceteris_paribus() function requires explainers created with specified 'data'.")
+    # need variables, if not provided, will be extracted from data
+    if (is.null(variables))
+      variables <- intersect(colnames(explainer$data),
+                             colnames(observations))
+
+    variable_splits <- calculate_variable_splits(explainer$data, variables = variables, grid_points = grid_points)
+  }
+
+  # calculate profiles
+  profiles <- calculate_profiles(observations, variable_splits, model, predict_function)
+  profiles$`_label_` <- explainer$label
+
+  # add points of interests
+  observations$`_yhat_` <- predict_function(model, observations)
+
+  # prepare final object
+  attr(profiles, "observations") <- observations
+  class(profiles) = c("ceteris_paribus_explainer", "data.frame")
+  profiles
+}
